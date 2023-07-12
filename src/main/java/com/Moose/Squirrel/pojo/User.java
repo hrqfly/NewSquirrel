d7f3156d89ba147d7d9339e1cfa662b91d08471d
moose <hrqfly@outlook.com>
2023-07-13 00:11:11 +0800

    新建开发包

---
+++ src/main/java/com/Moose/Squirrel/pojo/User.java
@@ -0,0 +1,14 @@
+package com.Moose.Squirrel.pojo;
+
+import lombok.Data;
+
+/**
+ * @author hrq
+ * @date 23/7/13
+ */
+
+@Data
+public class User {
+    private Integer userId;
+    private String userName;
+}
