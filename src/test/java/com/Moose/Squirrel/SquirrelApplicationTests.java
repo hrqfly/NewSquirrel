88be4dfeae9ab191b99481c3cac743a02f59e9c2
moose <hrqfly@outlook.com>
2023-07-12 23:30:36 +0800

    上传初始项目

---
+++ src/test/java/com/Moose/Squirrel/SquirrelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Moose.Squirrel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquirrelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
